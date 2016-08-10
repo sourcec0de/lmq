d0a3cd303bb6241e1cf2ef10d2813c4038410818
Zwb <zwb.ict@gmail.com>
2016-08-10 11:35:49 +0800

    feat<queue>: define queue struct and ctor

--- queue.go
+++ queue.go
@@ -1,8 +1,65 @@
 package lmq
 
+import "sync"
+
 type Queue interface {
 	Open(conf *Config) error
 	LoadTopicMeta(topic string) error
 	PutMessage(msg *Message, topic string)
 	Close() error
+}
+
+var (
+	queueManager struct {
+		sync.RWMutex
+		queueMap map[string]Queue
+	}
+)
+
+type queue struct {
+	conf *Config
+
+	sync.RWMutex
+	topicMap map[string]Topic
+
+	backendStorage BackendStorage
+}
+
+func NewQueue(path string, conf *Config) (Queue, error) {
+	queueManager.RLock()
+	q, ok := queueManager.queueMap[path]
+	queueManager.RUnlock()
+	if ok {
+		return q, nil
+	}
+
+	queueManager.Lock()
+
+	q, ok = queueManager.queueMap[path]
+	if ok {
+		queueManager.Unlock()
+		return q, nil
+	}
+
+	q = &queue{
+		conf:     conf,
+		topicMap: make(map[string]Topic),
+	}
+
+	var backendStorage BackendStorage
+	var err error
+	switch {
+	case "Bolt":
+		backendStorage, err = NewBoltBackendStorage(path, conf)
+	case "Lmdb":
+		backendStorage, err = NewLmdbBackendStorage(path, conf)
+	}
+	if err != nil {
+		return nil, err
+	}
+	q.backendStorage = backendStorage
+	queueManager.queueMap[path] = q
+	queueManager.Unlock()
+
+	return q, nil
 }
