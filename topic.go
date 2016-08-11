e9d1e19139f89ad376d2903eb97472e6e11e6ddc
Zwb <zwb.ict@gmail.com>
2016-08-11 18:23:34 +0800

    feat<topic>: add topic struct, LoadTopicMeta

--- topic.go
+++ topic.go
@@ -2,4 +2,23 @@ package lmq
 
 type Topic interface {
 	PutMessages(msgs []*Message)
+	LoadTopicMeta() error
+}
+
+type topic struct {
+	name  string
+	queue Queue
+}
+
+func NewTopic(name string, queue Queue) Topic {
+	t := &topic{
+		name:  name,
+		queue: queue,
+	}
+	return t
+}
+
+func (t *topic) LoadTopicMeta() error {
+	bs := t.queue.BackendStorage()
+	return bs.LoadTopicMeta(t.name)
 }
