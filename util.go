e204948bb98033267ca6ab7590d52e4cc204a9fa
Zwb <zwb.ict@gmail.com>
2016-08-12 10:46:31 +0800

    feat<util>: add uInt64ToBytes, bytesToUint64, uInt64ToString

--- util.go
+++ util.go
@@ -1,6 +1,10 @@
 package lmq
 
-import "sync"
+import (
+	"encoding/binary"
+	"strconv"
+	"sync"
+)
 
 type WaitGroupWrapper struct {
 	sync.WaitGroup
@@ -12,4 +16,18 @@ func (w *WaitGroupWrapper) Wrap(cb func()) {
 		cb()
 		w.Done()
 	}()
+}
+
+func uInt64ToBytes(i uint64) []byte {
+	var buf = make([]byte, 8)
+	binary.BigEndian.PutUint64(buf, i)
+	return buf
+}
+
+func bytesToUInt64(buf []byte) uint64 {
+	return binary.BigEndian.Uint64(buf)
+}
+
+func uInt64ToString(i uint64) string {
+	return strconv.FormatUint(i, 10)
 }
