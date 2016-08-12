edd3c84b5cb56c03e644df037b8eca4c05b5d288
Zwb <zwb.ict@gmail.com>
2016-08-12 15:23:47 +0800

    fix<ping_pong_buffer>: add sync.Mutex to guard critical region

--- ping_pong_buffer.go
+++ ping_pong_buffer.go
@@ -1,20 +1,26 @@
 package lmq
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 type PingPongBuffer struct {
 	cache0       []*Message
 	cache1       []*Message
 	currentCache *[]*Message
+	sync.Mutex
 
-	flushThreshold uint64
+	flushThreshold int
 	bgFlush        chan bool
 	flushInterval  time.Duration
 
-	handler func(topic string, msgs []*Message)
+	exitChan chan int
+
+	handler func(msgs []*Message)
 }
 
-func NewPingPongBuffer(size uint64, flushInterval time.Duration, handler func(topic string, msgs []*Message)) *PingPongBuffer {
+func NewPingPongBuffer(exitChan <-chan int, size int, flushInterval time.Duration, handler func(msgs []*Message)) *PingPongBuffer {
 	ppb := &PingPongBuffer{
 		cache0:         make([]*Message, size),
 		cache1:         make([]*Message, size),
@@ -24,9 +30,49 @@ func NewPingPongBuffer(size uint64, flushInterval time.Duration, handler func(to
 		handler:        handler,
 	}
 	ppb.currentCache = &ppb.cache0
+
 	return ppb
 }
 
 func (ppb *PingPongBuffer) Put(msg *Message) {
+	ppb.Lock()
+	defer ppb.Unlock()
 
+	cache := *ppb.currentCache
+	cache = append(cache, msg)
+	if len(cache) >= ppb.flushThreshold {
+		ppb.bgFlush <- true
+	}
+}
+
+func (ppb *PingPongBuffer) Flush() {
+	flushTicker := time.NewTicker(ppb.flushInterval)
+
+	for {
+		select {
+		case <-ppb.bgFlush:
+		case <-flushTicker.C:
+			ppb.Lock()
+			if len(*ppb.currentCache) > 0 {
+				flushCache := ppb.currentCache
+				if ppb.currentCache == &ppb.cache0 {
+					ppb.currentCache = &ppb.cache1
+				} else {
+					ppb.currentCache = &ppb.cache0
+				}
+				ppb.Unlock()
+				ppb.handler(*flushCache)
+				*flushCache = nil
+			} else {
+				ppb.Unlock()
+			}
+		case <-ppb.exitChan:
+			goto exit
+		}
+	}
+
+exit:
+	flushTicker.Stop()
+	ppb.cache0 = nil
+	ppb.cache1 = nil
 }
