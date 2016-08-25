13e479562eb46cb739d0ce6b05c29b22cd98d89a
Zwb <zwb.ict@gmail.com>
2016-08-25 14:26:39 +0800

    test<lmq_suite_test>: add setup and cleanup

--- lmq_suite_test.go
+++ lmq_suite_test.go
@@ -1,6 +1,8 @@
 package lmq_test
 
 import (
+	"os"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
@@ -11,3 +13,13 @@ func TestLmq(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Lmq Suite")
 }
+
+var _ = BeforeSuite(func() {
+	err := os.Mkdir("./queue_data", 0755)
+	Expect(err).NotTo(HaveOccurred())
+})
+
+var _ = AfterSuite(func() {
+	err := os.RemoveAll("./queue_data")
+	Expect(err).NotTo(HaveOccurred())
+})
