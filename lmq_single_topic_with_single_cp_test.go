2614cadee33cc256108f0fa938bb253f95d72fa5
Zwb <zwb.ict@gmail.com>
2016-10-08 17:12:01 +0800

    test<lmq_single_topic_with_siggnle_cp_test>: test Close

--- lmq_single_topic_with_single_cp_test.go
+++ lmq_single_topic_with_single_cp_test.go
@@ -82,6 +82,7 @@ var _ = Describe("LmqSingleTopicWithSingleCp", func() {
 				}
 
 				time.Sleep(5 * time.Second)
+				aproducer.Close()
 
 				tc, err := consumer.ConsumeTopic(topicName, 0)
 				Expect(err).NotTo(HaveOccurred())
