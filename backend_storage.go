e769732ad985efbfafa767ca1c4d068f0278806b
Zwb <zwb.ict@gmail.com>
2016-10-11 11:15:07 +0800

    refactor<backend_storage>: rename ScanMessages => ConsumeMessages

--- backend_storage.go
+++ backend_storage.go
@@ -3,7 +3,7 @@ package lmq
 type BackendStorage interface {
 	OpenTopic(topic, groupID string, flag int) Topic
 	PersistMessages(topic Topic, msgs []*Message)
-	ScanMessages(topic Topic, groupID string, msgs chan<- *[]byte)
+	ConsumeMessages(topic Topic, groupID string, msgs chan<- *[]byte)
 	Stat(topic Topic) *TopicStat
 	CloseTopic(topic Topic)
 	Close()
